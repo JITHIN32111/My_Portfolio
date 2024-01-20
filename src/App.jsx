8c5dd785c00c571c5589510018161b0843753f40
Jithin C <jith97972@gmail.com>
2024-01-20 08:09:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-blue-500'>hallo world</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [ "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {
+      colors:{
+      primeColor:"#8873ef",
+      headingColor:"#081e21",
+      smallTextColor:"#193256"  
+      }
+    },
+  },
+  plugins: [],
+}
+
