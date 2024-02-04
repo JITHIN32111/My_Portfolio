d233384e48132420971f615f0141caf691beef11
Jithin C <jith97972@gmail.com>
2024-02-04 12:42:09 +0530

    test demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,88 @@
-import React, { useEffect } from "react";
-import Aos from "aos";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Hero from "./UI/Hero";
-import Portfolio from "./UI/Portfolio";
-import AboutMe from "./UI/AboutMe";
+// import React, { useEffect } from "react";
+// import Aos from "aos";
+// import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import Hero from "./UI/Hero";
+// import Portfolio from "./UI/Portfolio";
+// import AboutMe from "./UI/AboutMe";
+// import Expertise from "./UI/Expertise";
+// function App() {
+//   useEffect(() => {
+//     Aos.init();
+//   }, []);
+//   return (
+//     // max-width: 1170px;
+//     // padding-left: 60px;
+//     // padding-right: 60px;
+//     // margin: auto;
+//     <div>
+//       <Header />
+//       <main>
+//         <Hero />
+//         <AboutMe/>
+//         <Portfolio/>
+//         <Expertise/>
+//       </main>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+
 function App() {
-  useEffect(() => {
-    Aos.init();
-  }, []);
   return (
-    // max-width: 1170px;
-    // padding-left: 60px;
-    // padding-right: 60px;
-    // margin: auto;
-    <div>
-      <Header />
-      <main>
-        <Hero />
-        <AboutMe/>
-        <Portfolio/>
-      </main>
-      <Footer />
+    <div className="px-16 py-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="bg-gray-50 shadow-xl rounded-lg">
+          <div className="flex flex-col">
+            <h1>Haiha</h1>
+            <p className="text-[12px] px-4">
+              {" "}
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt
+              possimus at fugit! Corporis delectus voluptas ratione ad
+              reiciendis quia{" "}
+            </p>
+          </div>
+        </div>
+        <div className="bg-gray-50 shadow-xl rounded-lg">
+          <div className="flex flex-col">
+            <h1>Haiha</h1>
+            <p className="text-sm px-4">
+              {" "}
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt
+              possimus at fugit! Corporis delectus voluptas ratione ad
+              reiciendis quia{" "}
+            </p>
+          </div>
+        </div>
+        <div className="bg-gray-50 shadow-xl rounded-lg">
+          <div className="flex flex-col">
+            <h1>Haiha</h1>
+            <p className="text-sm px-4">
+              {" "}
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt
+              possimus at fugit! Corporis delectus voluptas ratione ad
+              reiciendis quia{" "}
+            </p>
+          </div>
+        </div>
+        <div className="bg-gray-50 shadow-xl rounded-lg">
+          <div className="flex flex-col">
+            <h1>Haiha</h1>
+            <p className="text-sm px-4">
+              {" "}
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt
+              possimus at fugit! Corporis delectus voluptas ratione ad
+              reiciendis quia{" "}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
-// import React from 'react'
-
-// function App() {
-//   return (
-//     <div>
-//       <div className='bg-black text-white w-full h-full fixed top-0 left-0 '>
-//         <div className='absolute top-1/2 left-1/2 text-white '>sssssssss</div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App
---
+++ src/UI/Expertise.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import backend from "../assets/images/icons/backend.png";
+import wenDesign from "../assets/images/icons/webDesign.png";
+import webDevelopment from "../assets/images/icons/webDevelopment.png";
+import fullstack from "../assets/images/icons/fullStack.png";
+
+function Expertise() {
+  return (
+    <div id="Expertise">
+      <div className="container pt-10">
+        <div className="text-center text-headingColor text-[2rem] font-[700]">
+          <h1>My Expertise</h1>
+        </div>
+        <div className="flex flex-col  pt-10 gap-y-4 gap-x-2 sm:flex-row">
+          <div className="flex flex-col  py-4 px-1 ml-4 items-center justify-start rounded-lg bg-neutral-50 shadow-xl ">
+            <figure className="shadow-xl">
+              <img className="w-10 h-10 " src={wenDesign} alt="" />
+            </figure>
+            <h1 className="text-headingColor pt-10 font-bold text:xl sm:text-md md:text-lg  lg:text-xl">
+              Responsive Web Design
+            </h1>
+            <p className="font-[500] italic line-clamp-5 text-wrap sm:line-clamp-4 sm:px-4 px-3 py-1 text-sm lg:text-[15px] leading-7 mt-2">
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequuntur soluta, cum atque neque debitis consequatur illo
+              ipsum{" "}
+            </p>
+          </div>
+          <div className="flex flex-col py-4 px-1 ml-4 items-center justify-start rounded-lg bg-neutral-50 shadow-xl ">
+            <figure className="shadow-xl">
+              <img className="w-10 h-10 " src={webDevelopment} alt="" />
+            </figure>
+            <h1 className="text-headingColor pt-10 font-bold text-xl">
+              Web Development
+            </h1>
+            <p className="font-[500] pl-2 py-1 text-sm lg:text-[15px] leading-7 mt-2">
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequuntur soluta, cum atque neque debitis consequatur illo
+              ipsum{" "}
+            </p>
+          </div>
+
+          <div className="flex flex-col py-4 px-1 ml-4 items-center justify-start rounded-lg bg-neutral-50 shadow-xl ">
+            <figure className="shadow-xl">
+              <img className="w-10 h-10 " src={fullstack} alt="" />
+            </figure>
+            <h1 className="text-headingColor pt-10 font-bold text-xl">
+              Fullstack Development
+            </h1>
+            <p className="font-[500] pl-2 py-1 text-sm lg:text-[15px] leading-7 mt-2">
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequuntur soluta, cum atque neque debitis consequatur illo
+              ipsum{" "}
+            </p>
+          </div>
+
+          <div className="flex flex-col py-4 px-1 ml-4 items-center justify-start rounded-lg bg-neutral-50 shadow-xl ">
+            <figure className="shadow-xl">
+              <img className="w-10 h-10 " src={backend} alt="" />
+            </figure>
+            <h1 className="text-headingColor pt-10 font-bold text-xl">
+              Backend Development
+            </h1>
+            <p className="font-[500] pl-2 py-1 text-sm lg:text-[15px] leading-7 mt-2">
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequuntur soluta, cum atque neque debitis consequatur illo
+              ipsum{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Expertise;
